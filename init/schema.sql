339b0634955cebfbfd3d32073013411bb9c51f19
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-06-03 09:03:52 -0300

    feat: initialize

---
+++ init/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS BOOKS (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "title" TEXT NOT NULL,
+  "author" TEXT NOT NULL,
+  "total_chapters" INTEGER NOT NULL,
+  "read_chapters" INTEGER NOT NULL
+);
---
+++ src/main/server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def home():
+  return jsonify({"hello": "world"})
